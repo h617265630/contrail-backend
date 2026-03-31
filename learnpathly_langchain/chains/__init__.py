ca1781cb66a57d123d490b0ea3f993abac16a4fd
burn <burn@burndeMacBook-Air.local>
2026-04-01 00:06:07 +0800

    Add admin dashboard and convert AIpath to submodule
    - Add admin dashboard with stats, user, resource, path, analytics APIs
    - Convert AIpath directory to git submodule pointing to contrail-ai repo
    - Add .gitignore for Python pycache files
    - Remove old learnpathly_langchain files (replaced by AIpath submodule)

--- learnpathly_langchain/chains/__init__.py
+++
@@ -1,6 +0,0 @@
-from .generator import run_generator
-from .intent import run_intent
-from .planner import run_planner
-from .tutorial_research import run_tutorial_research
-
-__all__ = ["run_intent", "run_planner", "run_generator", "run_tutorial_research"]
--- learnpathly_langchain/path_gen/__init__.py
+++
@@ -1,4 +0,0 @@
-from .service import generate_learning_path
-from .schemas import LearningPathResult, LearningNode, SubNode, ResourceLink
-
-__all__ = ["generate_learning_path", "LearningPathResult", "LearningNode", "SubNode", "ResourceLink"]
